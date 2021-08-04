09ba7fda3e486dd4ca9ba0d001791a0bd9990284
Jatin <jatindahiya100.com>
2021-08-04 16:11:39 +0530

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function () {
 
     $('#loader').slideUp(1000);
-    $('.wrapper').fadeIn(1000);
+    $('.wrapper').delay(1000).fadeIn(1000);
 });
 
 $(document).ready(function(){
