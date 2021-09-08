091ac5d48449e5e7de3149360c88a6d46bc604db
Jatin <jatindahiya100.com>
2021-09-08 14:33:39 +0530

    Initial commit

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,16 @@
 // Loading header
 $('#header').load("header.html");
+
+$(document).on("click", "#minus", function () {
+    var x = $('#qt').val();
+    if (x > 1) {
+        var minus = --x;
+        $('#qt').val(minus);
+    }
+});
+
+$(document).on("click", "#plus", function () {
+    var x = $('#qt').val();
+    var add = ++x;
+    $('#qt').val(add);
+});
