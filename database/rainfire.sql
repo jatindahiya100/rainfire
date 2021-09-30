ab6939e2395f8fed57d6c349fb8776e2c20096ac
Jatin <jatindahiya100.com>
2021-09-30 19:52:53 +0530

    Initial commit

--- database/rainfire.sql
+++ database/rainfire.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Sep 27, 2021 at 01:17 PM
+-- Generation Time: Sep 30, 2021 at 04:22 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -46,6 +46,40 @@ INSERT INTO `heroimage` (`id`, `heading`, `banner_url`, `linked_to`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_ID` varchar(255) NOT NULL,
+  `placed_on` datetime NOT NULL DEFAULT current_timestamp(),
+  `order_Amt` float NOT NULL,
+  `payment_Status` char(20) NOT NULL DEFAULT 'Pending',
+  `cName` varchar(255) NOT NULL,
+  `cMail` varchar(255) NOT NULL,
+  `cMobile` varchar(50) NOT NULL,
+  `cState` varchar(255) NOT NULL,
+  `cDistrict` varchar(255) NOT NULL,
+  `pincode` varchar(255) NOT NULL,
+  `shipping_Address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_Details`
+--
+
+CREATE TABLE `order_Details` (
+  `unique_ID` int(11) NOT NULL,
+  `order_ID` varchar(255) NOT NULL,
+  `p_ID` varchar(255) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `item_price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
@@ -55,28 +89,27 @@ CREATE TABLE `products` (
   `p_Name` varchar(255) NOT NULL,
   `thumbnail` longtext NOT NULL,
   `p_Price` int(11) NOT NULL,
-  `original_price` int(11) NOT NULL,
   `discount` int(11) NOT NULL,
-  `money_saved` int(11) NOT NULL,
   `added_on` date NOT NULL DEFAULT current_timestamp(),
   `category` varchar(50) NOT NULL,
   `rating` int(11) NOT NULL,
   `stock` varchar(20) NOT NULL DEFAULT 'In Stock',
-  `description` longtext NOT NULL
+  `description` longtext NOT NULL,
+  `archieve` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `p_ID`, `p_Name`, `thumbnail`, `p_Price`, `original_price`, `discount`, `money_saved`, `added_on`, `category`, `rating`, `stock`, `description`) VALUES
-(1, '180921105239', 'Rainfire Helio Comet', 'products_gallery/HP-BH10_TT_2_From_TPS_technologies_1024x1024.webp', 12999, 18849, 45, 5850, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.'),
-(2, '180921105638', 'Comet-X Hemisphere', 'products_gallery/sennheiser-hd-350bt-on-ear-wireless-headphones-black-1_1.jpg', 19000, 23750, 25, 4750, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.'),
-(3, '180921105955', 'Rainfire Beast', 'products_gallery/61zrP+qT7EL._SL1500_.jpg', 9999, 11199, 12, 1200, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.'),
-(4, '180921110335', 'Stylo mafia thunder', 'products_gallery/rPWYcTBnCS3PRBSgf7Fkam-1200-80.jpg', 12900, 14448, 12, 1548, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.'),
-(5, '180921110623', 'Rainfire-HeX Handsfree', 'products_gallery/Clean-Living-EarPods-with-3-SDL934922275-1-d49eb.jpeg', 2200, 3190, 45, 990, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.'),
-(6, '180921111032', 'Axe Stormbreaker - Rainfire', 'products_gallery/784dee9669f28275a21d24655f87a31d.jpeg', 5500, 6710, 22, 1210, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.'),
-(7, '180921111320', 'Rainfire Bettle Box', 'products_gallery/860a3eee-482d-41b3-8b45-fbfd73199fac1623844708853-realme-Buds-Air-2-with-Active-Noise-Cancellation-ANC-Bluetoo-1.webp', 12000, 15600, 30, 3600, '2021-09-18', 'Headphones', 4, 'Out Of Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.');
+INSERT INTO `products` (`id`, `p_ID`, `p_Name`, `thumbnail`, `p_Price`, `discount`, `added_on`, `category`, `rating`, `stock`, `description`, `archieve`) VALUES
+(1, '180921105239', 'Rainfire Helio Comet', 'products_gallery/HP-BH10_TT_2_From_TPS_technologies_1024x1024.webp', 12999, 45, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0),
+(2, '180921105638', 'Comet-X Hemisphere', 'products_gallery/sennheiser-hd-350bt-on-ear-wireless-headphones-black-1_1.jpg', 19000, 25, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0),
+(3, '180921105955', 'Rainfire Beast', 'products_gallery/61zrP+qT7EL._SL1500_.jpg', 9999, 12, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0),
+(4, '180921110335', 'Stylo mafia thunder', 'products_gallery/rPWYcTBnCS3PRBSgf7Fkam-1200-80.jpg', 12900, 12, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0),
+(5, '180921110623', 'Rainfire-HeX Handsfree', 'products_gallery/Clean-Living-EarPods-with-3-SDL934922275-1-d49eb.jpeg', 2200, 45, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0),
+(6, '180921111032', 'Axe Stormbreaker - Rainfire', 'products_gallery/784dee9669f28275a21d24655f87a31d.jpeg', 5500, 22, '2021-09-18', 'Headphones', 4, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0),
+(7, '180921111320', 'Rainfire Bettle Box', 'products_gallery/860a3eee-482d-41b3-8b45-fbfd73199fac1623844708853-realme-Buds-Air-2-with-Active-Noise-Cancellation-ANC-Bluetoo-1.webp', 12000, 30, '2021-09-18', 'Headphones', 5, 'In Stock', 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo rem non odio voluptas ab neque tempora reprehenderit velit dolores ipsam ipsum beatae adipisci, earum necessitatibus illo. Doloribus, nobis provident ratione nihil quo ullam? Vel illum architecto natus quis ea atque quasi est, possimus velit unde qui! Incidunt accusantium dignissimos quos.', 0);
 
 -- --------------------------------------------------------
 
@@ -168,6 +201,20 @@ ALTER TABLE `heroimage`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_ID`);
+
+--
+-- Indexes for table `order_Details`
+--
+ALTER TABLE `order_Details`
+  ADD PRIMARY KEY (`unique_ID`),
+  ADD KEY `order_ID` (`order_ID`),
+  ADD KEY `p_ID` (`p_ID`);
+
+--
 -- Indexes for table `products`
 --
 ALTER TABLE `products`
@@ -199,26 +246,39 @@ ALTER TABLE `heroimage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
+-- AUTO_INCREMENT for table `order_Details`
+--
+ALTER TABLE `order_Details`
+  MODIFY `unique_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `p_images`
 --
 ALTER TABLE `p_images`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `specificaions`
 --
 ALTER TABLE `specificaions`
-  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `order_Details`
+--
+ALTER TABLE `order_Details`
+  ADD CONSTRAINT `order_Details_ibfk_1` FOREIGN KEY (`order_ID`) REFERENCES `orders` (`order_ID`),
+  ADD CONSTRAINT `order_Details_ibfk_2` FOREIGN KEY (`p_ID`) REFERENCES `products` (`p_ID`);
 
 --
 -- Constraints for table `p_images`
